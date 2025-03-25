06c9e4c4ce6ce1df281ba3f30bc19f17fec587b1
UlysseAll <u_allard@live.concordia.ca>
2025-03-25 13:37:03 -0400

    store function implementation I'm a bit confuse if we should use global of pass vlues direclty in the in function definition. In the assignment report it gives an example of store(name, value) so I dont know if we have to only use this formating or if we can pass more things in the function definition. Initially I had coded it with passign a bunch of values in func definition but I changed it in the end to just store(name, value). Tomorrow i have my original lab session and I'll attend to ask what is expected. regardless, the logic will not change just how variable are access so Alex you should be good to go in terms of lookup().

--- lab3/main.py
+++ lab3/main.py
@@ -6,8 +6,10 @@ process_lock = threading.Lock()
 
 time_counter = 1  # Shared Variable Start global time at 1s
 time_running = True
-
-
+memory_used = 0
+main_memory = []
+disk_memory = []
+command_index = 0
 def timer():
     global time_counter 
     while time_running:
@@ -78,9 +80,25 @@ def read_commands_file(filename="commands.txt"):
 
 def read_memconfig_file(filename="memconfig.txt"):
     with open(filename, 'r') as file:
-        memory_space = file.read().strip()
+       memory_space = file.read().strip()
 
     return int(memory_space)
+
+def store(variable, value):
+    global time_counter, commands, command_index, memory_used, main_memory, disk_memory
+
+    if memory_used < memory_space: 
+        main_memory.append(variable)
+        main_memory.append(value) 
+        command_index += 2
+        memory_used += 1
+    else:
+        disk_memory.append(commands[command_index+1])
+        disk_memory.append(commands[command_index+2]) 
+        command_index += 2
+    time_counter += 1
+    time.sleep(1)
+    return 
 
 def fair_share_scheduler(quantum, processes, file):
     global time_counter
@@ -169,8 +187,10 @@ def run_process(user, process_id, execution_time, process, file):
 
 
 if __name__ == "__main__":
+    commands = read_commands_file()
+    nb_cores, nb_processes, processes = read_process_file()
+    memory_space = read_memconfig_file()
 
-    quantum, processes = read_input_file()
     with open("output.txt", 'w') as file:
 
         scheduler_thread = threading.Thread(target=fair_share_scheduler, args=(quantum,processes,file))
